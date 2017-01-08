3f5d49d6f4dcaf22d344266277101e889f4cc76b
Nate Alcedo <ndaljr@gmail.com>
2017-01-08 19:37:30 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './sass/index.sass';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
